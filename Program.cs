7f3207f45421039775ada48dd17db059e4274890
Peter Allen Webb <peterallenwebb@gmail.com>
2015-09-12 23:44:14 -0400

    preliminary work

---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+
+using Cloo;
+using System.Diagnostics;
+
+
+namespace NQueens
+{
+	public struct QueenInit
+	{
+		public char blah; // TODO
+	}
+
+	public struct QueenResult
+	{
+		public long count;
+	}
+
+	class MainClass
+	{
+		public const int NumQueens = 8;
+		public const int Spread = 44;
+
+		public static void Main (string[] args)
+		{
+			var platforms = ComputePlatform.Platforms;
+
+			if (platforms.Count > 1) 
+				Console.WriteLine("More than one platform available, maybe branch out...");
+
+			var platform = platforms[0];
+			var properties = new ComputeContextPropertyList (platform);
+			var context = new ComputeContext(platform.Devices, properties, null, IntPtr.Zero);
+		
+			var init = new QueenInit[Spread];
+			var initBuffer = new ComputeBuffer<QueenInit>(context, 
+				                                          ComputeMemoryFlags.ReadOnly | ComputeMemoryFlags.CopyHostPointer, 
+				                                          init);
+
+			var results = new QueenResult[Spread];
+			var resultsBuffer = new ComputeBuffer<QueenResult>(context, 
+				                                               ComputeMemoryFlags.WriteOnly, 
+			                                                   results.Length);
+
+			string queenKernelSource = GetQueenKernelSource();
+			var program = new ComputeProgram(context, queenKernelSource);
+
+			try 
+			{ 
+				program.Build(null, null, null, IntPtr.Zero);
+			}
+			catch 
+			{
+				string log = program.GetBuildLog(platform.Devices[0]);
+				Console.WriteLine (log);
+			}
+
+			ComputeKernel kernel = null;
+
+			try 
+			{
+				kernel = program.CreateKernel("place");
+			}
+			catch 
+			{
+				string log = program.GetBuildLog(platform.Devices[0]);
+
+
+				Console.WriteLine (log);
+			}
+				
+
+			kernel.SetMemoryArgument(0, initBuffer);
+			kernel.SetMemoryArgument(1, resultsBuffer);
+
+
+			ComputeEventList eventList = new ComputeEventList();
+
+			var sw = new Stopwatch();
+			sw.Start();
+
+			var commands = new ComputeCommandQueue(context, context.Devices[0], ComputeCommandQueueFlags.None);
+			commands.Execute(kernel, null, new long[] { Spread }, null, eventList);
+			commands.ReadFromBuffer(resultsBuffer, ref results, false, eventList);
+			commands.Finish();
+
+			sw.Stop();
+
+			Console.WriteLine(sw.ElapsedMilliseconds / 1000.0);
+
+			foreach (var r in results) {
+				Console.WriteLine(r.count);
+			}
+		}
+
+		public static string GetQueenKernelSource()
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			var resourceName = "NQueens.queen_kernel.c";
+
+			using (var stream = assembly.GetManifestResourceStream (resourceName))
+			using (var reader = new StreamReader(stream)) 
+			{
+				return reader.ReadToEnd();
+			}
+		}
+
+		public static void PrintContextDetails(ComputeContext context)
+		{
+			Console.WriteLine("[HOST]");
+			Console.WriteLine(Environment.OSVersion);
+
+			Console.WriteLine();
+			Console.WriteLine("[OPENCL PLATFORM]");
+
+			ComputePlatform platform = context.Platform;
+
+			Console.WriteLine("Name: " + platform.Name);
+			Console.WriteLine("Vendor: " + platform.Vendor);
+			Console.WriteLine("Version: " + platform.Version);
+			Console.WriteLine("Profile: " + platform.Profile);
+			Console.WriteLine("Extensions:");
+
+			foreach (string extension in platform.Extensions)
+				Console.WriteLine(" + " + extension);
+
+			Console.WriteLine();
+
+			Console.WriteLine("Devices:");
+
+			foreach (ComputeDevice device in context.Devices)
+			{
+				Console.WriteLine("\tName: " + device.Name);
+				Console.WriteLine("\tVendor: " + device.Vendor);
+				Console.WriteLine("\tDriver version: " + device.DriverVersion);
+				Console.WriteLine("\tOpenCL version: " + device.Version);
+				Console.WriteLine("\tCompute units: " + device.MaxComputeUnits);
+				Console.WriteLine("\tGlobal memory: " + device.GlobalMemorySize + " bytes");
+				Console.WriteLine("\tLocal memory: " + device.LocalMemorySize + " bytes");
+				Console.WriteLine("\tImage support: " + device.ImageSupport);
+				Console.WriteLine("\tExtensions:");
+
+				foreach (string extension in device.Extensions)
+					Console.WriteLine("\t + " + extension);
+			}
+		}
+	}
+}
---
+++ queen_kernel.c
@@ -0,0 +1,87 @@
+﻿
+struct queenInit
+{
+	char blah; // TODO
+};
+
+struct queenResult
+{
+	long solutions;
+};
+
+enum Step { Place, Remove };
+
+typedef long qint;
+
+__constant const int q = 15;
+
+__constant const qint dodge = (1 << q) - 1;
+
+__kernel void place(__global struct queenInit * init, 
+                    __global struct queenResult * results)
+{
+  int index = get_global_id(0);
+
+  long n = 0;
+  qint masks[q];
+  int col = 0;
+  qint rook = 0;
+  qint add = 0;
+  qint sub = 0;
+  enum 
+  Step step = Place;
+
+  qint mask = dodge & ~(rook | (add >> col) | (sub >> ((q - 1) - col)));
+  qint rext = 0;
+
+  if (mask == 0)
+  	step = Remove;
+
+while (true)
+{
+  if (step == Remove)
+  {
+    if (col == 0)
+    {
+      results[index].solutions = n;
+      return;
+    }
+
+    --col;
+    mask = masks[col];
+  }
+
+  rext = mask & -mask;
+  rook ^= rext;
+  add  ^= rext << col;
+  sub  ^= rext << (q - 1 - col);
+
+  if (step == Place)
+  {
+    masks[col] = mask;
+    ++col;
+
+    if (col != q)
+    {
+      mask = dodge & ~(rook | (add >> col) | (sub >> ((q - 1) - col)));
+
+      if (mask == 0)
+        step = Remove;
+    }
+    else
+    {
+      n += 1;
+      step = Remove;
+    }
+  }
+  else
+  {
+    mask ^= rext;
+
+    if (mask == 0)
+      step = Remove;
+    else
+      step = Place; 
+  }
+}
+}
